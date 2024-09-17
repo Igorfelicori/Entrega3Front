55c2d208e569cf1e4c73a4fe038bf07c8f116d95
Igorfelicori <igorfelicori2006@gmail.com>
2024-09-16 21:32:12 -0300

    Criando e estilizando componentes

---
+++ challenge3/src/components/Container/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./container.css"
+import Header from '../Header'
+import Footer from '../Footer'
+
+type ContainerProps = {
+    children: React.ReactNode,
+    img: string
+}
+
+const Container: React.FC <ContainerProps> = ({children, img}) => {
+  return (
+    <div style={{ backgroundImage: `url(/assets/${img})` }} className="container">
+      <Header></Header>
+      {children}
+      <Footer></Footer>
+      </div>
+  )
+}
+
+export default Container
---
+++ challenge3/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./header.css"
+
+
+const Header: React.FC = () => {
+  return (
+    <header className="nav_container">
+        {/* Barra de navegação */}
+        <Link className="main_title" to="/">GIF</Link>
+        {/* Título principal */}
+        <Link className="creators" to="/criadores">CRIADORES</Link>
+        {/* Link para a página de criadores */}
+    </header>
+  )
+}
+
+export default Header
