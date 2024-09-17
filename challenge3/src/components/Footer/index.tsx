5ab7cddd6c5f5769a7ced5eb414d2770de620c44
GustavoVasconcelos045 <gustavovasribeiro@hotmail.com>
2024-09-16 21:48:47 -0300

    estilizando footer

--- challenge3/src/components/Footer/index.tsx
+++ challenge3/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 
 const Footer: React.FC = () => {
   return (
-    <footer className='text_footer'>© 2024 - Todos os Direitos Reservados.</footer>
+    <footer className='text_footer'> - GIF Motors Todos os direitos reservados © 2024.</footer>
   )
 }
 
